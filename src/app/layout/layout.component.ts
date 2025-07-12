88b21978365b1b43407cb8cf815b12939d87b47f
dlimiyassin <dlimiyassine08@gmail.com>
2025-07-12 18:45:45 +0100

    fix the fucking scrolling that made my mind blowing

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,6 +1,7 @@
 import { Component, ChangeDetectorRef } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { fader, slider, transform, pageFlip } from '../route.animations';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
@@ -14,7 +15,16 @@ import { fader, slider, transform, pageFlip } from '../route.animations';
   ],
 })
 export class LayoutComponent {
-  constructor(private cdr: ChangeDetectorRef) {}
+
+constructor(private router: Router, private cdr: ChangeDetectorRef ){
+  this.router.events
+    .pipe(filter(e => e instanceof NavigationEnd))
+    .subscribe(() => {
+      const container = document.querySelector('.content-container');
+      if (container) container.scrollTo(0, 0);
+    });
+}
+
 
   prepareRoute(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
